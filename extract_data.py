612b11012d498e8a726bba47b357421fcfe3bb89
Rainier Hardjanto <rainierwardhana@gmail.com>
2024-03-08 14:46:17 -0500

    Rainier's first comit this is a commit just to add all of the python files from Rainier's first edit. This is Rainier's first commit in the repo.

---
+++ extract_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import string
+
+x = pd.read_csv("billboard_100.csv")
+
+genius_characters = (
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789- "
+)
+
+
+def format_for_genius(string):
+    formatted_string = ""
+    for character in string:
+        if character in genius_characters:
+            formatted_string += character
+        elif character == "&":
+            formatted_string += "and"
+    return formatted_string
+
+
+def remove_features(string):
+    for i in range(len(string)):
+        if string[i : i + 9] == "featuring":
+            string = string[: i - 1]
+    return string
+
+
+def generate():
+    title = []
+    artists = []
+
+    for i in range(len(x)):
+        title.append(format_for_genius(x["Title"][i]))
+        artists.append(format_for_genius(remove_features(x["Artist(s)"][i])))
+
+    return title, artists
---
+++ test.py
@@ -0,0 +1,6 @@
+import requests
+
+# r = requests.get("https://genius.com/Lewis-capaldi-someone-you-loved-lyrics")
+r = requests.get("https://xkcd.com/353/")
+print(r.text)
+# print("\n\n")
