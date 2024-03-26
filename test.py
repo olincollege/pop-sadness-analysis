c6394041622c8c4fd934c702976f43ef8cf15f67
Will Young <ukjoma@gmail.com>
2024-03-26 14:22:24 -0400

    Deleted unnecessary files

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# r = requests.get("https://genius.com/Lewis-capaldi-someone-you-loved-lyrics")
-r = requests.get("https://xkcd.com/353/")
-print(r.text)
-# print("\n\n")
--- wikipedia-api.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-x = pd.read_csv("billboard_100.csv")
